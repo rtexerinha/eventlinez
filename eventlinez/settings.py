99823d1f25013e636961daa57e8fa53068998e14
Rafael Reuber <rafaelreuber@vortexz.com.br>
2020-10-17 16:54:28 -0300

    Remoção da configuração de produção para banco de dados

--- eventlinez/settings.py
+++ eventlinez/settings.py
@@ -84,14 +84,9 @@ WSGI_APPLICATION = 'eventlinez.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydjangodb',
-        'USER': 'postgres',
-        'PASSWORD': 'texera123',
-        'HOST': '50.116.31.214',
-        'PORT': '5432',
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + 'db.sqlite3',
     }
 }
 
