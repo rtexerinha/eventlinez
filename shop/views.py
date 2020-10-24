752744051bae625c7cdbe74b15e1f0a8261c6dd1
Rafael Reuber <rafaelreuber@vortexz.com.br>
2020-10-24 09:56:37 -0300

    Alteração do nome da view ProdCatDetail para snake_case #26

--- shop/views.py
+++ shop/views.py
@@ -34,7 +34,7 @@ def allProdCat(request, c_slug=None):
 	return render(request, 'shop/category.html', {'category': c_page, 'products': products})
 
 
-def ProdCatDetail(request, c_slug, product_slug):
+def product_category_detail(request, c_slug, product_slug):
 	try:
 		product = Product.objects.get(category__slug=c_slug, slug=product_slug)
 	except Exception as e:
