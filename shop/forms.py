b68733983c2af60049eb2de94ce4634c96b17163
Rafael Reuber <rafaelreuber@vortexz.com.br>
2020-10-28 23:16:27 -0300

    Remover campo email do formulário de cadastro de usuário. O username deve ser o proprio email para simplificar o cadastro de usuaŕios.

--- shop/forms.py
+++ shop/forms.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
 	first_name = forms.CharField(max_length=100, required=True)
 	last_name = forms.CharField(max_length=100, required=True)
-	email = forms.EmailField(max_length=254, help_text='eg. youremail@anyemail.com')
+	username = forms.EmailField(label="Email", max_length=254, help_text='eg. youremail@anyemail.com')
 
 	class Meta:
 		model = User
-		fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+		fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
