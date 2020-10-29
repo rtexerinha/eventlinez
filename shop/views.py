90393b5050c874549dc352938b6c858ee66bb5e8
brunojnd <bruno.dias@vortexz.com.br>
2020-10-28 22:25:26 -0300

    tela de login

--- shop/views.py
+++ shop/views.py
@@ -33,7 +33,7 @@ def product_event_detail(request, c_slug, event_slug):
         products_list = Event.objects.all().filter(available=True)
     except Exception as e:
         raise e
-    return render(request, 'shop/event.html', {'event': event, 'products_list': products_list})
+    return render(request, 'shop/event.html', {'event': event, 'products_list': products_listgi})
 
 
 def signup_view(request):
