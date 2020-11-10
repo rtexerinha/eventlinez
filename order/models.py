2a52aeda55c787687864ccadd3b63e7eecde3f11
brunojnd <bruno.dias@vortexz.com.br>
2020-11-10 14:15:35 -0300

    [Fix #11]: Validando price de order para valores positivos

--- order/models.py
+++ order/models.py
@@ -3,36 +3,39 @@ from django.core.validators import MinValueValidator
 
 
 class Order(models.Model):
-	token = models.CharField(max_length=250, blank=True)
-	total = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
-	emailAddress = models.EmailField(max_length=250, blank=True)
-	created = models.DateTimeField(auto_now_add=True)
-	billingName = models.CharField(max_length=250, blank=True)
-	billingAddress1 = models.CharField(max_length=250, blank=True)
-	billingCity = models.CharField(max_length=250, blank=True)
-	billingPostcode = models.CharField(max_length=10, blank=True)
-	billingCountry = models.CharField(max_length=200, blank=True)
-	shippingName = models.CharField(max_length=250, blank=True)
-	shippingAddress1 = models.CharField(max_length=250, blank=True)
-	shippingCity = models.CharField(max_length=250, blank=True)
-	shippingPostcode = models.CharField(max_length=10, blank=True)
-	shippingCountry = models.CharField(max_length=200, blank=True)
+    token = models.CharField(max_length=250, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    emailAddress = models.EmailField(max_length=250, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=10, blank=True)
+    billingCountry = models.CharField(max_length=200, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostcode = models.CharField(max_length=10, blank=True)
+    shippingCountry = models.CharField(max_length=200, blank=True)
 
-	class Meta:
-		ordering = ['-created']
+    class Meta:
+        ordering = ['-created']
 
-	def __str__(self):
-		return str(self.id)
+    def __str__(self):
+        return str(self.id)
 
 
 class OrderItem(models.Model):
-	event = models.CharField(max_length=250)
-	quantity = models.IntegerField(validators=[MinValueValidator(0)])
-	price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='GBP Price')
-	order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    event = models.CharField(max_length=250)
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                verbose_name='GBP Price',
+                                validators=[MinValueValidator(0)])
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
-	def sub_total(self):
-		return self.quantity * self.price
+    def sub_total(self):
+        return self.quantity * self.price
 
-	def __str__(self):
-		return self.event
+    def __str__(self):
+        return self.event
