280675deeae77284ed7c7e148bef9e7f3be7c0b5
brunojnd <bruno.dias@vortexz.com.br>
2020-11-27 13:44:15 -0300

    Criando a view de events

--- event/views.py
+++ event/views.py
@@ -1,10 +1,27 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+
+from event.models import Event
+from order.models import Order
 
 
 @login_required
 def order_promoter(request):
-    if not request.user.username:
-        return redirect('signin_promoter')
-    else:
+    if request.user.is_authenticated:
+        promoter = request.user.promoter.id
+        events = Event.objects.filter(promoter=promoter)
+        # promoter_orders = Order.objects.filter(orderitem=events.id)
         return render(request, 'orders_promoter.html')
+        # return render(request, 'orders_promoter.html', {'promoter_orders': promoter_orders})
+    else:
+        return render(request, 'accounts/signin_customer.html')
+
+
+@login_required
+def events_promoter(request):
+    if request.user.is_authenticated:
+        promoter = request.user.promoter.id
+        events = Event.objects.filter(promoter=promoter)
+        return render(request, 'events_promoter.html', {'events': events})
+    else:
+        return render(request, 'accounts/signin_customer.html')
