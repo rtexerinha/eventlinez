a74696cf7c03fa9ea6d7245fff40dbb5e0f67493
brunojnd <bruno.dias@vortexz.com.br>
2020-11-27 13:47:23 -0300

    Criando rota e linkando a mesma no painel do promoter

--- eventlinez/urls.py
+++ eventlinez/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 import shop
-from event.views import order_promoter
+from event.views import order_promoter, events_promoter
 from customer.views import signout_view, signup_view, signin_view, signup_view_promoter, signin_view_promoter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin/promoter/', order_promoter, name='order_promoter'),
+    path('admin/promoter/events', events_promoter, name='events_promoter'),
     path('shop/', include('shop.urls')),
     path('about/', shop.views.about, name='about'),
     path('contact/', shop.views.contact, name='contact'),
