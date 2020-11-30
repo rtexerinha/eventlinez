2af9057fb3b0b594a8d64ccf88758bee2a740e36
brunojnd <bruno.dias@vortexz.com.br>
2020-11-30 16:21:55 -0300

    removendo fone da listagem de ordem

--- cart/views.py
+++ cart/views.py
@@ -49,89 +49,92 @@ def add_cart(request, product_id):
 
 
 def cart_detail(request, total=0, counter=0, cart_items=None):
-    try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, active=True)
-        for cart_item in cart_items:
-            total += (((cart_item.event.unit_price * Decimal(EVENTLINEZ_FEE)) +
-                       cart_item.event.unit_price) * cart_item.quantity)
-            counter += cart_item.quantity
-    except Cart.DoesNotExist:
-        logger.error("The cart doest not exist.")
-        pass
+    if not request.user.is_authenticated:
+        return redirect('signin')
+    else:
+        try:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, active=True)
+            for cart_item in cart_items:
+                total += (((cart_item.event.unit_price * Decimal(EVENTLINEZ_FEE)) +
+                           cart_item.event.unit_price) * cart_item.quantity)
+                counter += cart_item.quantity
+        except Cart.DoesNotExist:
+            logger.error("The cart doest not exist.")
+            pass
 
-    stripe.api_key = settings.STRIPE_SECRET_KEY
-    stripe_total = int(total * 100)
-    description = 'New Order'
-    data_key = settings.STRIPE_PUBLISHABLE_KEY
-    if request.method == 'POST':
-        token = request.POST['stripeToken']
-        email = request.POST['stripeEmail']
-        billing_name = request.POST['stripeBillingName']
-        billing_address1 = request.POST['stripeBillingAddressLine1']
-        billingcity = request.POST['stripeBillingAddressCity']
-        billing_postcode = request.POST['stripeBillingAddressZip']
-        billing_country = request.POST['stripeBillingAddressCountryCode']
-        shipping_name = request.POST['stripeShippingName']
-        shipping_address1 = request.POST['stripeShippingAddressLine1']
-        shippingcity = request.POST['stripeShippingAddressCity']
-        shipping_postcode = request.POST['stripeShippingAddressZip']
-        shipping_country = request.POST['stripeShippingAddressCountryCode']
-        # promoter = request.POST['promoter']
-        try:
-            customer = stripe.Customer.create(email=email, source=token)
-            logger.info("create customer")
-            charge = stripe.Charge.create(
-                amount=stripe_total,
-                currency="usd",
-                description=description,
-                customer=customer.id
-            )
-        except stripe.error.CardError as err:
-            # return HttpResponse(status=400, content=err.user_message)
-            content = err.user_message
-            return render(request, 'order/error_cart.html', {'content': content})
-        logger.info("Creating the order")
-        try:
-            order = Order.objects.create(
-                token=token,
-                payment_code=charge.stripe_id,
-                total=total,
-                emailAddress=email,
-                billingName=billing_name,
-                billingAddress1=billing_address1,
-                billingCity=billingcity,
-                billingPostcode=billing_postcode,
-                billingCountry=billing_country,
-                shippingName=shipping_name,
-                shippingAddress1=shipping_address1,
-                shippingCity=shippingcity,
-                shippingPostcode=shipping_postcode,
-                shippingCountry=shipping_country,
-                # promoter=promoter
-            )
-            for order_item in cart_items:
-                oi = OrderItem(
-                    event=order_item.event,
-                    quantity=order_item.quantity,
-                    price=order_item.event.unit_price,
-                    promo_code=order_item.promo_code,
-                    order=order,
-                    # promoter=order_item.event.promoter
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe_total = int(total * 100)
+        description = 'New Order'
+        data_key = settings.STRIPE_PUBLISHABLE_KEY
+        if request.method == 'POST':
+            token = request.POST['stripeToken']
+            email = request.POST['stripeEmail']
+            billing_name = request.POST['stripeBillingName']
+            billing_address1 = request.POST['stripeBillingAddressLine1']
+            billingcity = request.POST['stripeBillingAddressCity']
+            billing_postcode = request.POST['stripeBillingAddressZip']
+            billing_country = request.POST['stripeBillingAddressCountryCode']
+            shipping_name = request.POST['stripeShippingName']
+            shipping_address1 = request.POST['stripeShippingAddressLine1']
+            shippingcity = request.POST['stripeShippingAddressCity']
+            shipping_postcode = request.POST['stripeShippingAddressZip']
+            shipping_country = request.POST['stripeShippingAddressCountryCode']
+            # promoter = request.POST['promoter']
+            try:
+                customer = stripe.Customer.create(email=email, source=token)
+                logger.info("create customer")
+                charge = stripe.Charge.create(
+                    amount=stripe_total,
+                    currency="usd",
+                    description=description,
+                    customer=customer.id
                 )
-                oi.save()
-                event = Event.objects.get(id=order_item.event.id)
-                event.stock = int(order_item.event.stock - order_item.quantity)
-                event.save()
-                order_item.delete()
-                logger.info("The order has been created")
-            send_mail.delay(order.id)
-            return redirect('order:thanks', order.id)
-        except ObjectDoesNotExist:
-            return HttpResponse(status=400, content="Page errada")
+            except stripe.error.CardError as err:
+                # return HttpResponse(status=400, content=err.user_message)
+                content = err.user_message
+                return render(request, 'order/error_cart.html', {'content': content})
+            logger.info("Creating the order")
+            try:
+                order = Order.objects.create(
+                    token=token,
+                    payment_code=charge.stripe_id,
+                    total=total,
+                    emailAddress=email,
+                    billingName=billing_name,
+                    billingAddress1=billing_address1,
+                    billingCity=billingcity,
+                    billingPostcode=billing_postcode,
+                    billingCountry=billing_country,
+                    shippingName=shipping_name,
+                    shippingAddress1=shipping_address1,
+                    shippingCity=shippingcity,
+                    shippingPostcode=shipping_postcode,
+                    shippingCountry=shipping_country,
+                    # promoter=promoter
+                )
+                for order_item in cart_items:
+                    oi = OrderItem(
+                        event=order_item.event,
+                        quantity=order_item.quantity,
+                        price=order_item.event.unit_price,
+                        promo_code=order_item.promo_code,
+                        order=order,
+                        # promoter=order_item.event.promoter
+                    )
+                    oi.save()
+                    event = Event.objects.get(id=order_item.event.id)
+                    event.stock = int(order_item.event.stock - order_item.quantity)
+                    event.save()
+                    order_item.delete()
+                    logger.info("The order has been created")
+                send_mail.delay(order.id)
+                return redirect('order:thanks', order.id)
+            except ObjectDoesNotExist:
+                return HttpResponse(status=400, content="Page errada")
 
-    return render(request, 'cart.html', dict(cart_items=cart_items, total=total, counter=counter,
-                                             data_key=data_key, stripe_total=stripe_total, description=description))
+        return render(request, 'cart.html', dict(cart_items=cart_items, total=total, counter=counter,
+                                                 data_key=data_key, stripe_total=stripe_total, description=description))
 
 
 def cart_remove(request, product_id):
