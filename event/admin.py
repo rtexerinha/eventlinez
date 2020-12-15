55c9c60fe5aad8c3acdc33cd0c4d14f4c804b391
brunojnd <bruno.dias@vortexz.com.br>
2020-12-15 15:51:41 -0300

    Add ticket ao admin de events

--- event/admin.py
+++ event/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Event, Promoter
+from .models import Category, Event, Promoter, Ticket
 
 
 @admin.register(Category)
@@ -18,3 +18,15 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ['name', 'unit_price', 'stock', 'event_address', 'available', 'event_date', 'promoter']
     # list_editable = ['price', 'stock', 'available']
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['created_at']
+    # list_editable = ['price', 'stock', 'available']
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
