eba9c6cca1a904049ebf3f5d5220087f26d3f641
brunojnd <bruno.dias@vortexz.com.br>
2020-12-29 01:04:42 -0300

    Organizando ordens das views

--- event/views.py
+++ event/views.py
@@ -20,26 +20,6 @@ def event_list(request):
 
 
 @login_required(login_url='/promoter/account/login/')
-def tickets_list(request):
-    events = Event.objects.filter(promoter=request.user.promoter).order_by('-created')
-    tickets = Ticket.objects.filter(event__promoter=request.user.promoter).order_by('-id')
-    selected_event = None
-    if request.method == "POST":
-        event_id = request.POST.get('events_choice')
-        if event_id:
-            selected_event = Event.objects.get(pk=event_id)
-            tickets = tickets.filter(event=selected_event)
-    paginator = Paginator(tickets, 6)
-    page = int(request.GET.get('page', '1'))
-    try:
-        tickets = paginator.page(page)
-    except (EmptyPage, InvalidPage):
-        tickets = paginator.page(paginator.num_pages)
-    data = {'tickets': tickets, 'events': events, 'selected_event': selected_event}
-    return render(request, 'ticket_list.html', data)
-
-
-@login_required(login_url='/promoter/account/login/')
 def event_create(request):
     if request.method == 'POST':
         form = NewEvent(request.POST, request.FILES)
@@ -68,6 +48,26 @@ def event_remove(request, event_id):
     event = get_object_or_404(Event, id=event_id)
     event.delete()
     return redirect('events_promoter')
+
+
+@login_required(login_url='/promoter/account/login/')
+def tickets_list(request):
+    events = Event.objects.filter(promoter=request.user.promoter).order_by('-created')
+    tickets = Ticket.objects.filter(event__promoter=request.user.promoter).order_by('-id')
+    selected_event = None
+    if request.method == "POST":
+        event_id = request.POST.get('events_choice')
+        if event_id:
+            selected_event = Event.objects.get(pk=event_id)
+            tickets = tickets.filter(event=selected_event)
+    paginator = Paginator(tickets, 6)
+    page = int(request.GET.get('page', '1'))
+    try:
+        tickets = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        tickets = paginator.page(paginator.num_pages)
+    data = {'tickets': tickets, 'events': events, 'selected_event': selected_event}
+    return render(request, 'ticket_list.html', data)
 
 
 @login_required(login_url='/promoter/account/login/')
