0feae56f748d415f3b4eb896b7813de4698419fb
brunojnd <brunojndias@gmail.com>
2021-02-05 17:30:17 -0300

    ajuste text help ssn

--- customer/forms.py
+++ customer/forms.py
@@ -16,7 +16,7 @@ class SignUpFormPromoter(forms.Form):
     address = forms.CharField(max_length=255)
     city = forms.CharField(max_length=250)
     zip = forms.CharField(max_length=11)
-    social_security = forms.CharField(max_length=12, help_text='Social security information is used for taxes purposes. Your information will not be shared with any company.')
+    social_security = forms.CharField(max_length=12, help_text='SSN information is only used for Taxes purposes.')
     phone = forms.CharField(max_length=12)
 
     password1 = forms.CharField(
