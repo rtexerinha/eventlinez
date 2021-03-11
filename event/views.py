2f53be179927ecf1d197be236790f18859fa8d32
andrelopes <andrelopessfla@gmail.com>
2021-03-11 19:58:28 -0300

    EVENLINEZ-T-32

--- event/views.py
+++ event/views.py
@@ -10,7 +10,8 @@ from django.utils.translation import ugettext
 
 from event.forms import EventForm
 from event.models import Event, Ticket
-
+from .models import Promoter
+from .forms import PromoterForm
 
 @login_required(login_url='/promoter/account/login/')
 def event_list(request):
@@ -84,7 +85,7 @@ def tickets_excel(request, event_id=None):
     props_title = {'bold': True, 'font_size': 14, 'align': 'center',
                    'valign': 'vcenter', 'font_name': 'Arial'}
 
-    props_header = {'bold': True, 'font_size': 10, 'align': 'center',  'valign': 'vcenter',
+    props_header = {'bold': True, 'font_size': 10, 'align': 'center', 'valign': 'vcenter',
                     'color': '#171717', 'bg_color': '#F4F4F4', 'font_name': 'Arial'}
 
     props_price = {'num_format': '[$$-409]#,##0.00', 'font_size': 10, 'align': 'right', 'color': '#171717',
@@ -118,7 +119,7 @@ def tickets_excel(request, event_id=None):
                                                                        'customer__last_name'
                                                                        ).order_by('-id')
     else:
-        tickets = Ticket.objects.filter(event__promoter=request.user.promoter).\
+        tickets = Ticket.objects.filter(event__promoter=request.user.promoter). \
             values_list('id', 'event__name', 'order_item__price', 'created_at', 'customer__first_name',
                         'customer__last_name').order_by('-id')
 
@@ -143,3 +144,21 @@ def tickets_excel(request, event_id=None):
     book.close()
     output.seek(0)
     return response
+
+
+@login_required
+def update_promoter(request):
+    user_id = request.user.id
+    promoter = Promoter.objects.get(user_id=user_id)
+    form = PromoterForm(instance=promoter)
+
+    if request.method == 'POST':
+        form = PromoterForm(request.POST, instance=promoter)
+
+        if form.is_valid():
+            form.save()
+            return redirect('events_promoter')
+        else:
+            return render(request, 'update_promoter.html', {'form': form})
+    elif request.method == 'GET':
+        return render(request, 'update_promoter.html', {'form': form})
