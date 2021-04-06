3bd191282b3ace4edd8dbb0f72e3154972442a5b
andrelopes <andrelopessfla@gmail.com>
2021-04-06 20:36:59 -0300

    adicionado o caminho para pasta templates

--- eventlinez/settings.py
+++ eventlinez/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'order',
 ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -70,7 +72,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'shop', 'templates/'),
                  os.path.join(BASE_DIR, 'cart', 'templates/'),
-                 os.path.join(BASE_DIR, 'order', 'templates/')],
+                 os.path.join(BASE_DIR, 'order', 'templates/'),
+                 os.path.join(BASE_DIR, '', 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
