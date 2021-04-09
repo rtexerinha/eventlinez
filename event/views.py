6d5653f42544626875eff503350ee675122f41ad
andrelopes <andrelopessfla@gmail.com>
2021-04-09 15:01:06 -0300

    removido template de erro pois ja foi adicionado o modal

--- event/views.py
+++ event/views.py
@@ -50,12 +50,8 @@ def event_update(request, event_id):
 @login_required(login_url='/promoter/account/login/')
 def event_remove(request, event_id):
     event = get_object_or_404(Event, id=event_id)
-    tickets = Ticket.objects.filter(event_id=event_id)
-    if not tickets:
-        event.delete()
-        return redirect('events_promoter')
-    else:
-        return render(request, 'erro_event_delete.html', {'event': event})
+    event.delete()
+    return redirect('events_promoter')
 
 
 @login_required(login_url='/promoter/account/login/')
