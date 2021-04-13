c45ca1761a401b97940baa997c35dfc6ea3750a9
andrelopes <andrelopessfla@gmail.com>
2021-04-13 11:55:33 -0300

    adicionado rotas para promoter

--- eventlinez/urls.py
+++ eventlinez/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 import shop
 from shop.views import index
@@ -34,11 +35,21 @@ urlpatterns = [
     path('promoter/events/update/<int:event_id>/', event_update, name='update_event'),
     path('promoter/events/full_remove/<int:event_id>/', event_remove, name='remove_event'),
 
-    # Reset password views
+    # Reset password views customer
     path('account/reset_password/', auth_views.PasswordResetView.as_view(html_email_template_name='registration/password_reset_email.html'), name="password_reset"),
     path('account/reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('account/reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('account/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
+    # Reset password views promoter
+    path('account/reset_password/promoter',
+         auth_views.PasswordResetView.as_view(html_email_template_name='registration/password_reset_email_promoter.html', success_url = reverse_lazy('password_reset_done_promoter')),
+         name="password_reset_promoter"),
+    path('account/reset_password_sent/promoter', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done_promoter"),
+    path('account/reset/promoter/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(success_url = reverse_lazy('password_reset_complete_promoter')),
+         name="password_reset_confirm_promoter"),
+    path('account/reset_password_complete/promoter', auth_views.PasswordResetCompleteView.as_view(template_name = 'registration/password_reset_complete_promoter.html'),
+         name="password_reset_complete_promoter"),
 ]
 
 admin.site.site_header = 'Eventlinez'
