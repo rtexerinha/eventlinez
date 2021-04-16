0c67fbd3c05a5cce4a4199a2717886b5d63cce25
bruno.dias <bruno.dias@vortexz.com.br>
2021-04-16 13:27:17 -0300

    criando um arquivo com js que altera o nome do convidado, que serve tanto para a view de my-tickets, quanto para a view thanks(view de confirmação de compra)

---
+++ static/js/guest-name-ticket.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(document).on("blur",".input-data",function(){
+        let value=$(this).val();
+        let td=$(this).parent("td");
+        let type=td.data("type");
+        sendToServer(td.data("id"),value,type);
+    });
+
+    $(document).on("keypress",".input-data",function(e){
+        let key=e.which;
+        if(key===13){
+            let value=$(this).val();
+            let td=$(this).parent("td");
+            let type=td.data("type");
+            sendToServer(td.data("id"),value,type);
+        }
+    });
+
+    function sendToServer(id,value,type){
+        $.ajax({
+            url:"http://127.0.0.1:8000/order/ticket/save/",
+            type:"POST",
+            data:{id:id,type:type,value:value},
+        })
+        .done(function(response){
+            console.log(response);
+        })
+        .fail(function(){
+           console.log("Error Occured");
+        });
+
+    }
+
+});
