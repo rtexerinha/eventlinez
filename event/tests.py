f7cf848fc662431587a34b08aa900d1f2d8a0822
Rafael Reuber <rafaelreuber@vortexz.com.br>
2021-06-16 13:07:35 -0300

    Remoção do teste unitário test_quantidade_de_tickets_ofertados_nao_pode_ser_menor_que_a_quantidade_de_tickets_vendidos

--- event/tests.py
+++ event/tests.py
@@ -16,19 +16,6 @@ class EventFormTest(TestCase):
         self.assertTrue(form.is_valid())
         form.save()
 
-    def test_quantidade_de_tickets_ofertados_nao_pode_ser_menor_que_a_quantidade_de_tickets_vendidos(self):
-        event = baker.make(Event, description="foo", stock=10)
-        order = baker.make(Order)
-        OrderItem.objects.create(event=event, quantity=3, price=100, amount=100, fee=10, order=order)
-        OrderItem.objects.create(event=event, quantity=3, price=100, amount=100, fee=10, order=order)
-
-        self.assertEqual(4, event.sales_info()['qtd_available'])
-        self.assertEqual(6, event.sales_info()['qtd_sould'])
-
-        form = EventForm({"stock": 5}, instance=event)
-        self.assertFalse(form.is_valid())
-        self.assertEqual('Ticket quantity cannot be less than quantity sold', form.errors['stock'][0])
-
 
 class EventTicketTest(TestCase):
 
