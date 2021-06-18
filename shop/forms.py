7f9c183fca7a84bd88695efa39fa08ad41a9c324
brunozn <brunojndias@gmail.com>
2021-06-18 04:35:52 -0300

    view de contanto layout novo

--- shop/forms.py
+++ shop/forms.py
@@ -2,23 +2,22 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    # name = forms.CharField(required=True,
-    #                      label="Name",
-    #                       widget=forms.TextInput(attrs={'class': 'form-contact namef'}))
     mail = forms.EmailField(required=True,
-                            label="Email",
-                            widget=forms.TextInput(attrs={'class': 'form-contact'}))
+                            label="",
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}))
     cellphone = forms.CharField(required=True,
-                                label="cellphone",
-                                widget=forms.TextInput(attrs={'class': 'form-contact'}))
+                                label="",
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Cellphone'}))
     CHOICES = (('Event Feedback', 'Event Feedback',),
                ('Order issues', 'Order issues',),
                ('Create a new event', 'Create a new event',),)
     subject = forms.ChoiceField(choices=CHOICES,
-                                widget=forms.Select(choices=CHOICES, attrs={'class': 'select-form-contact'}))
+                                label="",
+                                widget=forms.Select(choices=CHOICES,
+                                                    attrs={'class': 'form-control', 'placeholder': 'Subject'}))
 
     message = forms.CharField(
         required=True,
-        widget=forms.Textarea(attrs={'class': 'form-contact'}),
-        label="Message"
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Message'}),
+        label=""
     )
