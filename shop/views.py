f094f7e8883ba62f31cfb6fe0f9fc7128a051f58
brunozn <brunojndias@gmail.com>
2021-06-19 16:19:44 -0300

    ajuste na home - voltando com banner antigo mas seguindo as dimensoes do novo layout

--- shop/views.py
+++ shop/views.py
@@ -24,7 +24,7 @@ def index(request, c_slug=None):
     # else:
     lis = lists_events(c_slug)
     page = pagination_home(request, lis)
-    detach = Event.objects.all().filter(available=True).order_by('-event_date').first()
+    detach = None
     return render(request, 'shop/home.html', {'detach': detach,
                                               'category': c_page,
                                               'events_futures': page[0],
