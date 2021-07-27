fdd663b29b78112e8135e5330224f9f680cea49a
brunozn <brunojndias@gmail.com>
2021-07-27 19:31:59 -0300

    uuid model e admin

--- ticket/admin.py
+++ ticket/admin.py
@@ -4,8 +4,8 @@ from ticket.models import Ticket
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ['id', 'event_ticket', 'customer', 'guest_name', 'created_at']
-    search_fields = ['customer__first_name', 'guest_name']
+    list_display = ['id', 'uuid', 'event_ticket', 'customer', 'guest_name', 'created_at']
+    search_fields = ['customer__first_name', 'guest_name', 'uuid']
 
     def has_delete_permission(self, request, obj=None):
         return False
--- ticket/models.py
+++ ticket/models.py
@@ -1,4 +1,11 @@
-from decimal import Decimal
+import uuid
+# from io import BytesIO
+#
+import qrcode
+import qrcode.image.svg
+# from PIL import Image
+# from django.core.files import File
+from io import BytesIO
 
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -14,6 +21,7 @@ class Ticket(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     guest_name = models.CharField(max_length=161, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
 
     def __str__(self):
         return "%s/%s" % (self.event_ticket.event.name, self.event_ticket.name)
