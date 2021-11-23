54b84a5b3b767134bdb1b706db5afa91d86dab04
brunozn <brunojndias@gmail.com>
2021-11-23 18:46:43 -0300

    refatoracao da funcao de export excel generica

--- static/js/ticket-export-excel.js
+++ static/js/ticket-export-excel.js
@@ -1,19 +1,6 @@
-function exportTicket() {
+function exportExcel() {
   const e = document.getElementById("eventsChoice");
   const eventId = e.options[e.selectedIndex].value;
-  if (eventId === "") {
-    let url = "excel/";
-    window.open(url, "_blank");
-  } else {
-    let url = `excel/${eventId}`;
-    window.open(url, "_blank");
-  }
-}
-
-function exportVendors() {
-  const e = document.getElementById("eventsChoice");
-  const eventId = e.options[e.selectedIndex].value;
-  console.log(e);
   if (eventId === "") {
     let url = "excel/";
     window.open(url, "_blank");
