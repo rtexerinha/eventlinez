9c1fed8cdc5ab030dd815cc837c358757af7204f
brunozn <brunojndias@gmail.com>
2022-04-13 16:47:21 -0300

    Listando infomações bankAccount no admin

--- promoter/admin.py
+++ promoter/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from promoter.models import Vendor
+from promoter.models import Vendor, BankAccount
 
 
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email', 'phone']
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ['bank_name', 'routing_number', 'last4', 'id_bank_account']
+
