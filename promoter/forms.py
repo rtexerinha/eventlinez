1e798755b5f1eb93428a45f983d81494b4535fb5
brunozn <brunojndias@gmail.com>
2022-10-09 17:43:01 -0300

    Removendo init não utlizado

--- promoter/forms.py
+++ promoter/forms.py
@@ -10,16 +10,9 @@ class BankAccountForm(ModelForm):
     class Meta:
         model = BankAccount
         fields = ['bank_name', 'account_number', 'routing_number']
-
         widgets = {'account_number': forms.TextInput(attrs={'placeholder': "000987654321"}),
                    'routing_number': forms.TextInput(attrs={'placeholder': "123456789"}),
                    }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.label_class = 'h5-promoter-modal'
-
 
 class PaymentForm(ModelForm):
     class Meta:
