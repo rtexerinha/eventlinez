c76ae1773eed0208f7b48b849e12bacc250c353c
andredevlopes <andrelopssfla@gmail.com>
2022-12-02 23:01:34 -0300

    chore: list event api

---
+++ promoter/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from event.models import Promoter, Event, Category
+from .models import Vendor
+from address.models import City
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class PromoterSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    email = serializers.CharField()
+    ssn = serializers.CharField()
+    phone = serializers.CharField()
+    address = serializers.CharField()
+    city = serializers.CharField()
+    zip = serializers.CharField()
+    user = UserSerializer()
+
+    class Meta:
+        model = Promoter
+        fields = '__all__'
+
+
+class CategoriaSerializers(serializers.Serializer):
+    name = serializers.CharField(max_length=250)
+    slug = serializers.SlugField(max_length=250)
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class CitySerializers(serializers.Serializer):
+    name = serializers.CharField()
+    state = serializers.CharField()
+
+    class Meta:
+        model = City
+        fields = '__all__'
+
+
+class EventSerializers(serializers.Serializer):
+    name = serializers.CharField()
+    slug = serializers.SlugField(max_length=250)
+    category = CategoriaSerializers()
+    description = serializers.CharField()
+    created = serializers.DateTimeField()
+    updated = serializers.DateTimeField()
+    event_date = serializers.DateTimeField()
+    address = serializers.CharField()
+    city = CitySerializers()
+    available = serializers.BooleanField(default=False)
+    image = serializers.ImageField()
+    image_sized = serializers.ImageField()
+    thumbnail = serializers.ImageField()
+
+    class Meta:
+        model = Event
+        fields = '__all__'
--- promoter/urls.py
+++ promoter/urls.py
@@ -7,7 +7,7 @@ from promoter.views import update_promoter, reset_password, signup_view_promoter
 from event.views import ticket_type_create, ticket_type_list, ticket_type_update, ticket_type_list_per_event
 from ticket.views import tickets_sold_list, tickets_excel, ticket_checkin, tickets_validate
 from event.views import event_list, event_create, event_remove, event_update
-from .api import CustomAuthToken, PromoterListAPIView
+from .api import CustomAuthToken, PromoterListAPIView, EventList
 
 urlpatterns = [
     path('account/create/', signup_view_promoter, name='signup_promoter'),
@@ -69,4 +69,7 @@ urlpatterns = [
     # auth
     path('api-token-auth/', CustomAuthToken.as_view()),
     path('api/information/', PromoterListAPIView.as_view()),
+
+    # event api
+    path('api/event/list', EventList.as_view()),
 ]
