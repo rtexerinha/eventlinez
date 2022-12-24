257548e7827c4cfc436457ec26fe84be5091f8dc
andredevlopes <andrelopssfla@gmail.com>
2022-12-24 12:44:36 -0300

    refactor: adjust list ticket api

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -4,9 +4,8 @@ from .models import Ticket
 
 class TicketSerializers(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    # order_item = models.ForeignKey('order.OrderItem', on_delete=models.PROTECT)
-    price = serializers.DecimalField(
-        max_digits=10, decimal_places=2, read_only=True)
+    event_ticket = serializers.CharField(read_only=True)
+    vendor = serializers.CharField(read_only=True)
     guest_name = serializers.CharField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     checkin_date = serializers.DateTimeField(read_only=True)
