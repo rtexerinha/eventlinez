04b3c7e3303b718928538e4db790454d1c60a3f8
andredevlopes <andrelopssfla@gmail.com>
2023-01-07 13:43:16 -0300

    feat: manual check-in api

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from ticket.views import ticket_qrcode, search_checkin, search_ticket_sold
-from .api import TicketListAPIView
+from .api import TicketListAPIView, TicketCheckinAPIView
 urlpatterns = [
     path('qrcode/', ticket_qrcode, name='ticket_qrcode'),
     path('search', search_checkin, name='search_guest'),
     path('search/ticket/sold/', search_ticket_sold, name='search_ticket_sold'),
     path('api/search', TicketListAPIView.as_view(), name='api_search_guest'),
+    path('api/checkin/<int:pk>', TicketCheckinAPIView.as_view(),
+         name='api_checkin_guest'),
 ]
