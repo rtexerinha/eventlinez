55ae534386ab758711d3f4223ef2c24028ce279d
andredevlopes <andrelopssfla@gmail.com>
2023-01-29 12:32:48 -0300

    refactor: including validation

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from datetime import timedelta
 from rest_framework import serializers
 from .models import Ticket
 
@@ -16,10 +18,28 @@ class TicketSerializers(serializers.Serializer):
         fields = '__all__'
 
     def update(self, instance, validated_data):
+
+        if not instance:
+            raise serializers.ValidationError({'error':
+                                               'Ticket does not belong to this promoter.'})
+
+        ticket_date_event = instance.event_ticket.event.event_date
+        deadline = (ticket_date_event + timedelta(hours=6)
+                    ).strftime("%Y-%m-%d %H:%M:%S")
+
+        if datetime.now().strftime("%Y-%m-%d %H:%M:%S") > deadline:
+            raise serializers.ValidationError(
+                {'error': 'Deadline to check in is over'})
+
+        if instance.checkin_date is not None:
+            raise serializers.ValidationError({'error':
+                                               'Ticket has already been validated!'})
         if 'checkin_date' in validated_data:
             if instance.checkin_date:
-                raise serializers.ValidationError('Ticket already checked in')
+                raise serializers.ValidationError(
+                    {'error': 'Ticket already checked in'})
             instance.checkin_date = validated_data['checkin_date']
             instance.save()
             return instance
-        raise serializers.ValidationError('Invalid checkin_date')
+
+        raise serializers.ValidationError({'error': 'Invalid checkin_date'})
