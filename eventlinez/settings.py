556a1bd3008bad3584bbed3bc78865195aa11bd9
brunozn <brunojndias@gmail.com>
2023-02-06 01:04:36 -0300

    refactor: Filtrar quando passar os dados campos da busca

--- eventlinez/settings.py
+++ eventlinez/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'wv3ra^smy9n*-^&oj%&h4ygkl7&#il(z$hc6$!*6ib+jhbx0rx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CKEDITOR_CONFIGS = {
     'default': {
