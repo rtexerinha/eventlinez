6b9f0e5be0da2d79ff191349d5d600278a67fa6d
Rafael Reuber <rafaelreuber@vortexz.com.br>
2023-02-24 09:15:46 +0000

    Adicionado filtro por state

--- promoter/urls.py
+++ promoter/urls.py
@@ -7,7 +7,7 @@ from promoter.views import update_promoter, reset_password, signup_view_promoter
 from event.views import ticket_type_create, ticket_type_list, ticket_type_update, ticket_type_list_per_event
 from ticket.views import tickets_sold_list, tickets_excel, ticket_checkin, tickets_validate
 from event.views import event_list, event_create, event_remove, event_update
-from .api import CustomAuthToken, PromoterListAPIView, EventAPIView, CategoryListAPIView, TicketAPIView,\
+from .api import CustomAuthToken, PromoterListAPIView, EventListAPIView, CategoryListAPIView, TicketAPIView,\
     EventUpdateAPIView, TicketUpdateAPIView, EventDetailsAPIView
 
 
@@ -73,7 +73,7 @@ urlpatterns = [
     path('api/information/', PromoterListAPIView.as_view()),
 
     # event api
-    path('api/event', EventAPIView.as_view()),
+    path('api/event/event', EventListAPIView.as_view()),
     path('api/event/update/<int:pk>', EventUpdateAPIView.as_view()),
     path('api/event/<int:pk>', EventDetailsAPIView.as_view()),
     path('api/categories', CategoryListAPIView.as_view()),
