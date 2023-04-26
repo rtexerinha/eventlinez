8557e7da82ea65854d6a4d0a1f268d2326001606
brunozn <brunojndias@gmail.com>
2023-04-26 20:32:47 -0300

    refactor: Refatorando as importações de TicketSold

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
 
 from ticket.views import ticket_qrcode, search_checkin, search_ticket_sold
-from .api import TicketListAPIView, TicketCheckinAPIView, TicketCheckinQrcodeAPIView, TicketDetailsAPIView
+from .api import TicketSoldListAPIView, TicketSoldCheckinAPIView, TicketSoldCheckinQrcodeAPIView, TicketSoldDetailsAPIView
+
+
 urlpatterns = [
     path('qrcode/', ticket_qrcode, name='ticket_qrcode'),
     path('search', search_checkin, name='search_guest'),
     path('search/ticket/sold/', search_ticket_sold, name='search_ticket_sold'),
-    path('api/search', TicketListAPIView.as_view(), name='api_search_guest'),
-    path('api/checkin/<int:pk>', TicketCheckinAPIView.as_view(),
+
+    # APIs de ticketSold
+    path('api/search', TicketSoldListAPIView.as_view(), name='api_search_guest'),
+    path('api/checkin/<int:pk>', TicketSoldCheckinAPIView.as_view(),
          name='api_checkin_guest'),
-    path('api/checkin/qrcode/<uuid:uuid>', TicketCheckinQrcodeAPIView.as_view(),
+    path('api/checkin/qrcode/<uuid:uuid>', TicketSoldCheckinQrcodeAPIView.as_view(),
          name='api_checkin_qrcode'),
-    path('api/search/<int:pk>', TicketDetailsAPIView.as_view(),
+    path('api/search/<int:pk>', TicketSoldDetailsAPIView.as_view(),
          name='api_search_id'),
 
 ]
