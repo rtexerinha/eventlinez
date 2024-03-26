fbe1c2256caf94581ab5ba956110e5cc1c3d94a7
brunozn <brunojndias@gmail.com>
2024-03-26 17:52:59 -0300

    Reenvio de email apos criar conta

--- account/models.py
+++ account/models.py
@@ -6,27 +6,27 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 
 
-# @receiver(post_save, sender=User)
-# def user_saved(sender, instance, **kwargs):
-#     if kwargs.get('created', False):
-#         # current_date = datetime.now(pytz.utc)
-#         unsent_tickets = Ticket.objects.filter(email=instance.username, account_required=True)
-#
-#         for freeticket in unsent_tickets:
-#             message = render_to_string('freeticket/email/freeticket-email.html',
-#                                        {'freeticket': freeticket, 'user': freeticket.guest_name})
-#
-#             email_free_ticket = EmailMessage(
-#                 subject="Eventlinez - New Free Ticket",
-#                 body=message,
-#                 from_email='noreply@eventlinez.com',
-#                 to=[instance.username],
-#             )
-#             email_free_ticket.content_subtype = "html"
-#
-#             output_pdf = freeticket.as_pdf()
-#             email_free_ticket.attach('ticket_{}.pdf'.format(freeticket.id), output_pdf, 'application/pdf')
-#
-#             email_free_ticket.send()
-#             freeticket.is_email_sent = True
-#             freeticket.save()
+@receiver(post_save, sender=User)
+def user_saved(sender, instance, **kwargs):
+    if kwargs.get('created', False):
+        # current_date = datetime.now(pytz.utc)
+        unsent_tickets = Ticket.objects.filter(email=instance.username, account_required=True)
+
+        for freeticket in unsent_tickets:
+            message = render_to_string('freeticket/email/freeticket-email.html',
+                                       {'freeticket': freeticket, 'user': freeticket.guest_name})
+
+            email_free_ticket = EmailMessage(
+                subject="Eventlinez - New Free Ticket",
+                body=message,
+                from_email='noreply@eventlinez.com',
+                to=[instance.username],
+            )
+            email_free_ticket.content_subtype = "html"
+
+            output_pdf = freeticket.as_pdf()
+            email_free_ticket.attach('ticket_{}.pdf'.format(freeticket.id), output_pdf, 'application/pdf')
+
+            email_free_ticket.send()
+            freeticket.is_email_sent = True
+            freeticket.save()
