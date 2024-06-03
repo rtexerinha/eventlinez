f34611035611a2c8eab1914f7fe30814f3f93bc1
brunozn <brunojndias@gmail.com>
2024-06-03 14:51:41 -0300

    fix: removendo roles dentro de evento

--- account/api.py
+++ account/api.py
@@ -39,16 +39,4 @@ class UserDetailAPI(APIView):
             roles = roles + list(partner_roles)
         data['roles'] = roles
 
-        events = Event.objects.filter(Q(partner__user=request.user) | Q(promoter__user=request.user)).distinct()
-        serialized_events = []
-        for event in events:
-            event_data = EventListSerializer(event).data
-            event_data["role"] = get_user_role(request.user, event)
-            serialized_events.append(event_data)
-
-        if len(serialized_events) <= 0:
-            return Response(
-                {"Error": "User not Promoter or Partner or Doorman"}, status=403)
-        data["events"] = serialized_events
-
         return Response(data)
