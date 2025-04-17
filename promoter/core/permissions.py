7b227ff08033fc4b8e393504ad6761deb5e9f437
Lucas Freire <lucasfreirel.dev@gmail.com>
2025-04-16 21:26:45 -0300

    fix(partner): resolved event details viewing logic for partners

--- promoter/core/permissions.py
+++ promoter/core/permissions.py
@@ -1,27 +1,18 @@
 from rest_framework.permissions import BasePermission
-from promoter.models import Event, Partner  # ajuste o import se necessário
+from promoter.models import Event, Partner 
 
 class IsEventParticipant(BasePermission):
-    """
-    Permite acesso ao evento se o usuário for:
-    - Superusuário
-    - Promotor do evento
-    - Partner vinculado ao evento
-    """
-
     def has_permission(self, request, view):
         user = request.user
-        event_id = view.kwargs.get("pk")
+        pk = view.kwargs.get("pk")
 
-        if user.is_staff:
-            return True
-
-        if not event_id:
+        if not pk:
             return False
 
-        # Verifica se é promoter do evento
-        is_promoter = Event.objects.filter(id=event_id, promoter__user=user).exists()
-        # Verifica se é partner do evento
-        is_partner = Partner.objects.filter(event_id=event_id, user=user).exists()
+        if Event.objects.filter(id=pk, promoter__user=user).exists():
+            return True
 
-        return is_promoter or is_partner
+        if Partner.objects.filter(user=user, event_id=pk, role="PARTNER").exists():
+            return True
+
+        return False
